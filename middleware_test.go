58a54713f2770b39b6c47c7792a05660d192a8e0
Ankur Srivastava <best.ankur@gmail.com>
2021-02-08 22:08:32 +0100

    fixed license headers
    Signed-off-by: Ankur Srivastava <best.ankur@gmail.com>

--- middleware_test.go
+++ middleware_test.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2020 Ankur Srivastava
+// Copyright (c) 2020-present Ankur Srivastava and Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
 package fiberprometheus
 
 import (
